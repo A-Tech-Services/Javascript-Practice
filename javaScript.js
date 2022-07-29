f34bfb2829fb2e785e60029c926b40a4c799d365
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-07-29 23:27:18 +0100

    update

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    document.getElementById("color").style.color = "green";
+}
+
+function changeFont(){
+    document.getElementById("color").style.fontSize = "50px";
+}
+
+document.getElementById("prac").innerHTML = "Javascript change me.";
