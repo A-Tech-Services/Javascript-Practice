69d2ac7b2119bff25c1b18c0adb0c7d46016fa20
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-07-30 01:20:21 +0100

    update

--- javaScript.js
+++ javaScript.js
@@ -7,3 +7,52 @@ function changeFont(){
 }
 
 document.getElementById("prac").innerHTML = "Javascript change me.";
+
+
+document.getElementById("sum").innerHTML = 5 + 6;
+
+
+
+document.write(5+9);
+
+document.write(" Hey this is just a test")
+
+console.log("Hey you")
+
+
+let x,y,z;
+
+x = 10;
+x = 55;
+y = 20;
+z = x + y;
+
+document.getElementById("calcu").innerHTML = z;
+
+
+let a = " come" + 3 + 4;
+document.write(a);
+
+
+const car = {
+    type: "Fiat",
+    model: "Bentley",
+    color: "Red"
+};
+
+car.tyre = "Flat tire";
+
+console.log(car)
+
+x = 10;
+y = 5;
+x += y;
+
+console.log(x);
+
+x++;
+console.log(x);
+
+x = Math.pow(x,2);
+console.log(x)
+
