9b08e594ab2cde6a1fefee5a18b40de76f7bf885
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2022-08-26 00:52:07 +0100

    update

--- javaScript.js
+++ javaScript.js
@@ -1,58 +1,51 @@
-function changeColor(){
-    document.getElementById("color").style.color = "green";
+// "use strict";
+
+// let message;
+// message = "Hello";
+
+// console.log(document.getElementById("id").innerHTML = message);
+
+// alert(message)
+
+
+// let admin, name;
+// name = "John";
+// admin = name;
+// alert(admin);
+// alert(2**53-1) 
+
+// let house = `${admin}`;
+// alert(house)
+
+// let age = prompt("How old are you? ");
+// alert(age);
+
+
+// let visitorName = prompt("What is your name? ");
+// alert(`My name is ${visitorName}, Thanks for having me here`);
+
+// alert("You must be atleast 18 or above to view this content!");
+
+// let ageConfirmation = prompt("Are you 18 or above?");
+// if(ageConfirmation < 18){
+//     alert("Sorry you are not allowed to view this content not until you are 18 already. Thanks.");
+// }
+// else{
+//     let name = prompt("What is your name?");
+//     alert(`Welcome ${name}.`)
+// }
+
+
+
+
+let girlfriend = "ibukunayo";
+
+let guest = prompt("What is your name?")
+
+if (guest == girlfriend){
+    alert("Welcome Atech Girlfriend!");
+} else if (guest == "motorola"){
+    alert(`Welcome motorola we were epecting ${girlfriend} we would have addressed you as Girlfriend`);
+}else{
+    alert("Sorry we dont know you!!!");
 }
-
-function changeFont(){
-    document.getElementById("color").style.fontSize = "50px";
-}
-
-document.getElementById("prac").innerHTML = "Javascript change me.";
-
-
-document.getElementById("sum").innerHTML = 5 + 6;
-
-
-
-document.write(5+9);
-
-document.write(" Hey this is just a test")
-
-console.log("Hey you")
-
-
-let x,y,z;
-
-x = 10;
-x = 55;
-y = 20;
-z = x + y;
-
-document.getElementById("calcu").innerHTML = z;
-
-
-let a = " come" + 3 + 4;
-document.write(a);
-
-
-const car = {
-    type: "Fiat",
-    model: "Bentley",
-    color: "Red"
-};
-
-car.tyre = "Flat tire";
-
-console.log(car)
-
-x = 10;
-y = 5;
-x += y;
-
-console.log(x);
-
-x++;
-console.log(x);
-
-x = Math.pow(x,2);
-console.log(x)
-
